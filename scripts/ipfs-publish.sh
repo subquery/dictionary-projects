41c6ca7022e163b06695a9b482124ea1d561c786
Lachlan McCrae <8554309+lachlan1m@users.noreply.github.com>
2022-09-12 12:05:43 +1200

    update shell script, add juno dictionary

--- scripts/ipfs-publish.sh
+++ scripts/ipfs-publish.sh
@@ -1,22 +1,21 @@
+#!/bin/sh
 if [ $# -eq 0 ]
   then
     echo "Pass token argument to use publishing script"
     exit 1
 fi
 
-echo "Running IPFS Publishing Script \n"
+printf 'Running IPFS Publishing Script \n'
 
-cd ../dictionaries
+cd ../dictionaries || exit
 
 for d in */ ; do
-    project=$(echo $d | sed 's:/*$::')
-    echo "Building and Publishing $project dictionary"
+    project=$(echo "$d" | sed 's:/*$::')
+    echo "Building and publishing $project dictionary"
 
-    (cd $d 
+    (cd "$d" || exit  
         yarn build
-        output=$(SUBQL_ACCESS_TOKEN=$1 subql publish)
-        ipfs=$(echo $output | awk '{print $NF}')
-        echo "$project IPFS hash: $ipfs \n"
+        SUBQL_ACCESS_TOKEN=$1 subql publish
     )
     
 done
